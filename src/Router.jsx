6a0d255ee986ed93e2c93a1919c3a29722a75383
meretamal <meretamal@uc.cl>
2021-12-23 20:12:30 -0300

    feat: add home page and navigation

---
+++ src/Router.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from '@pages/static/Home';
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ src/pages/static/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+
+export default function Home() {
+  return (
+    <Container>
+      <Typography>Home</Typography>
+    </Container>
+  );
+}
