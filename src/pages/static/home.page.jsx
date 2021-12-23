360d45a1e94152ab861e9e59fe2044b54780fe3b
meretamal <meretamal@uc.cl>
2021-12-23 20:22:06 -0300

    style: chane file naming

--- src/pages/static/home.page.jsx
+++ src/pages/static/home.page.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 
-export default function Home() {
+export default function HomePage() {
   return (
     <Container>
       <Typography>Home</Typography>
--- src/router.jsx
+++ src/router.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from '@pages/static/Home';
+import Home from '@pages/static/home.page';
 
 export default function Router() {
   return (
