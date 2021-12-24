83a889290aa10a43bd495e737a509f8e208590fa
meretamal <meretamal@uc.cl>
2021-12-24 16:14:01 -0300

    chore(cypress): add cypress config and setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,13 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    jest: true,
   },
   extends: [
     'plugin:react/recommended',
     'airbnb',
     'plugin:prettier/recommended',
+    'plugin:cypress/recommended',
   ],
   parserOptions: {
     ecmaFeatures: {
