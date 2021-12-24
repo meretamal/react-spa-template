8bce0c99295a8ba48ad252fe1c9b9ca3a46739ae
meretamal <meretamal@uc.cl>
2021-12-23 23:40:11 -0300

    refactor(store): delete unnecessary module

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import baseApi from '@api/base.api';
-import example from './example/example.slice';
 import theme from './theme/theme.slice';
 
 export default combineReducers({
-  example,
   theme,
   [baseApi.reducerPath]: baseApi.reducer,
 });
