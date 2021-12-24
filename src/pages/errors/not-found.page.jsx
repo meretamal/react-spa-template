3a15e9603288a967855566874dd67813ecea7003
meretamal <meretamal@uc.cl>
2021-12-24 14:30:14 -0300

    feat(pages): add a 404 error page

---
+++ src/pages/errors/not-found.page.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
+import { Link as RouterLink } from 'react-router-dom';
+import Hero from '@components/layout/hero.component';
+
+export default function NotFoundPage() {
+  return (
+    <Hero
+      sx={{
+        minHeight: {
+          xs: 'calc(100vh - 56px)',
+          sm: 'calc(100vh - 64px)',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        },
+      }}
+    >
+      <Typography variant="h1" textAlign="center">
+        <Box
+          component="span"
+          sx={{ color: 'primary.main', fontWeight: 'bold' }}
+        >
+          404
+        </Box>{' '}
+        ERROR
+      </Typography>
+      <Typography variant="h5" textAlign="center">
+        The page you are looking for does not exist
+      </Typography>
+      <Link to="/" component={RouterLink} sx={{ my: 2 }}>
+        <Typography variant="h6">Go to home</Typography>
+      </Link>
+    </Hero>
+  );
+}
--- src/router.jsx
+++ src/router.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Footer from '@components/layout/footer.component';
-import Home from '@pages/static/home.page';
+import HomePage from '@pages/static/home.page';
 import Navbar from '@components/layout/navbar.component';
+import NotFoundPage from '@pages/errors/not-found.page';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
       <Footer />
     </BrowserRouter>
